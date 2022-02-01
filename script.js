078e427f4745cc6a5888e5eb82b8b5aaa60a79e8
B-Devy <bertrandcafolla@hotmail.com>
2022-02-01 17:05:32 +0100

    day 02

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
-var body = document.querySelector('body');
-var bouton = document.querySelector('button');
-var creation = document.createElement('p');
-var num = 0;
-var bulle = document.createElement('div');
+const counterDisplay = document.querySelector('h3');
+let counter = 0;
 
-/*function comptage() { 
-    bulle.style.bottom = i + "px"; 
-    return i++;
- }*/
+const bubbleMaker = () => {
 
-bouton.addEventListener('click', () => {
+const bubble = document.createElement('span');
+bubble.classList.add('bubble');
+document.body.appendChild(bubble);
 
-    
-    body.appendChild(bulle);
-    bulle.classList.add('bulle');
-    bulle.style.bottom = i + "px"; 
-/*
-    for (var i = 0; i < 1500; i++) {
-        bulle.style.bottom = i + "px"; 
-    }*/
-    
-    function myMove() {
-        let id = null;
-      
-        let pos = 0;
-        clearInterval(id);
-        id = setInterval(frame, 5);
-        function frame() {
-          if (pos == 350) {
-            clearInterval(id);
-          } else {
-            pos++; 
-            bulle.style.top = pos + "px"; 
-            bulle.style.left = pos + "px"; 
-          }
-        }
-      }
+const size = Math.random() * 200 + 100 + "px";
+
+bubble.style.height = size;
+bubble.style.width = size;
+
+bubble.style.top = Math.random() *100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%";
+
+const plusMoins = Math.random() > 0.5 ? 1 : -1;
+
+bubble.style.setProperty('--left', Math.random() * 100 * plusMoins + "%");
 
 
-    /*
-    for (var i = 0; i < 1500; i ++) {
-        i++;
-    }
-    */
-    
+  
+/*----------Efface la bulle au clique------------*/
+bubble.addEventListener('click', () => {
+  counter++;
+  console.log(counter);
+  counterDisplay.textContent = counter;
+  bubble.remove()
 });
+
+/*-----Effacer la bulle----- */
+setTimeout(() => {
+  bubble.remove();
+}, 8000)
+
+};
+
+setInterval(bubbleMaker, 1000);
 
 
 
